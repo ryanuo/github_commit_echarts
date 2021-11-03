5a80b6a492945395b338c8dbc8383b2373a79894
rr210 <1577791638qq.com>
2021-11-03 22:58:14 +0800

    3d可视化

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// vue.config.js
+const path = require('path')
+module.exports = {
+  publicPath: './', // 文件加载设置为相对路径
+  outputDir: 'dist',
+  // lintOnSave: false, // 关闭eslint
+  // productionSourceMap: true, // 生产环境下css 分离文件
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+}
